c0a73c5da3b6a13f0b231ed5cb74d2462217af33
Akansha Rai <48284707+raiakansha@users.noreply.github.com>
2024-03-05 17:59:39 +0530

    made changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -74,7 +74,8 @@ const Header = () => {
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
         <div className="logo" onClick={() => navigate("/")}>
-          <img src={logo} alt="" />
+          {/* <img src={logo} alt="" /> */}
+          <h1 className="heading">CineMate</h1>
         </div>
         <ul className="menuItems">
           <li className="menuItem" onClick={() => navigationHandler("movie")}>
